cc79219962600a6a315164e4da483af0c390a1c4
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2025-02-18 12:39:37 -0500

    update contract for blockSenders

--- forge/src/Jackal.sol
+++ forge/src/Jackal.sol
@@ -30,6 +30,7 @@ abstract contract Jackal {
     event ResetEditors(address from, string for_address, string file_owner);
     event CreatedNotification(address from, string to, string contents, string private_contents);
     event DeletedNotification(address from, string notification_from, uint64 time);
+    event BlockedSenders(address from, string[] to_block);
 
     function getPrice() public view virtual returns (int256);
 
@@ -322,5 +323,13 @@ abstract contract Jackal {
         hasAllowance(from)
     {
         emit DeletedNotification(from, notification_from, time);
+    }
+
+    function blockSenders(string[] memory to_block) public {
+        blockSendersFrom(msg.sender, to_block);
+    }
+
+    function blockSendersFrom(address from, string[] memory to_block) public validAddress hasAllowance(from) {
+        emit BlockedSenders(from, to_block);
     }
 }
