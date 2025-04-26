27343271477fcf382a423038acdd3350cafc489e
marston <marston@jackallabs.io>
2025-04-26 01:33:42 -0400

    drawer upgrades

--- forge/src/StorageDrawer.sol
+++ forge/src/StorageDrawer.sol
@@ -4,22 +4,47 @@ pragma solidity ^0.8.26; // solidity ^0.8.0 recommended
 import {JackalInterface} from "./JackalInterface.sol"; // import bridge interface
 
 contract StorageDrawer {
+    struct FileDetails {
+        address owner;
+        string merkle;
+        uint start;
+    }
+
     JackalInterface internal jackalBridge; // variable for interface
-    mapping(address => string[]) public cabinet; // variable for file owners
+    mapping(address => FileDetails[]) public cabinet; // variable for file owners
 
     constructor(address _jackalAddress) {
         // constructor takes bridge address
         jackalBridge = JackalInterface(_jackalAddress); // use jackal bridge
     }
 
-    function upload(string memory merkle, uint64 filesize) public payable {
+    function upload(string memory merkle, uint64 filesize, uint value) public payable {
         // takes file and size
-        jackalBridge.postFileFrom{value: msg.value}(msg.sender, merkle, filesize, "", 73000); // call bridge for 200 years
+        jackalBridge.postFileFrom{value: value}(msg.sender, merkle, filesize, "", 73000); // call bridge for 200 years
         // method list https://github.com/JackalLabs/mulberry/blob/main/forge/src/JackalInterface.sol
-        cabinet[msg.sender].push(merkle); // record file owner
+
+        cabinet[msg.sender].push(FileDetails(msg.sender, merkle, block.number)); // record file owner
+    }
+
+    function uploadMany(string[] memory merkle, uint64[] memory filesize, uint[] memory values) public payable {
+        for (uint i=0; i<merkle.length; i++) {
+            upload(merkle[i], filesize[i], values[i]);
+        }
     }
 
     function fileAddress(address _addr, uint256 _index) public view returns (string memory) {
+        // takes address and index
+        require(_index < cabinet[_addr].length); // check file exists for address
+        return cabinet[_addr][_index].merkle; // return file
+    }
+
+    function fileStart(address _addr, uint256 _index) public view returns (uint) {
+        // takes address and index
+        require(_index < cabinet[_addr].length); // check file exists for address
+        return cabinet[_addr][_index].start; // return file
+    }
+
+    function fileDetails(address _addr, uint256 _index) public view returns (FileDetails memory) {
         // takes address and index
         require(_index < cabinet[_addr].length); // check file exists for address
         return cabinet[_addr][_index]; // return file
